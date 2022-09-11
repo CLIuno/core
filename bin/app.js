b2c374ce0c0dc05b043541ee893d089d2063a4fb
RuM <18311071+ru44@users.noreply.github.com>
2022-09-11 06:45:52 +0300

    add shelljs - inquirer

--- bin/app.js
+++ bin/app.js
@@ -9,21 +9,57 @@ inquirer
             type: 'list',
             name: 'framework',
             message: 'Choose the JS framework which you are using:',
-            choices: ['React', 'NextJS', 'Angular', 'Svelte', 'VueJS'],
-        },{
+            choices: ['VueJs', 'NuxtJS', 'React', 'NextJS', 'Angular', 'Svelte', ''],
+        }, {
             type: 'list',
             name: 'packageManager',
             message: 'Choose the package manager which you are using:',
             choices: ['npm', 'yarn'],
-        }
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter your name:',
+        },
+        {
+            type: 'password',
+            name: 'password',
+            message: 'Enter your password:',
+        },
+        {
+            type: 'editor',
+            name: 'bio',
+            message: 'Tell us about yourself:',
+        },
+        {
+            type: 'expand',
+            name: 'expandTest',
+            message: 'Conflict on `file.js`: ',
+            choices: [
+                {
+                    key: 'y',
+                    name: 'Overwrite',
+                    value: 'overwrite',
+                }],
+        },
+        {
+            type: 'checkbox',
+            name: 'checkboxTest',
+            message: 'Conflict on `file.js`: ',
+        },
+        {
+            type: 'confirm',
+            name: 'isConfirmed',
+            message: 'Are you sure you want to continue?',
+        },
     ])
     .then((answers) => {
-        // Use user feedback for... whatever!!
+        console.log(answers);
     })
     .catch((error) => {
         if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
+            console.log("Prompt couldn't be rendered in the current environment");
         } else {
-            // Something else went wrong
+            console.log("Something else went wrong");
         }
     });
