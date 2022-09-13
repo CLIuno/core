da9d06ed8b6bf5ddf746de20f5e09b2716f2e9c0
AlBaraa Habiballah <al.habiballah@tamkeentech.sa>
2022-09-13 17:57:09 +0300

    add links

---
+++ bin/links.js
@@ -0,0 +1,20 @@
+let links = new Map([
+    ['VueJS','https://vuejs.org/'],
+    ['NuxtJs','https://nuxtjs.org/'],
+    ['ReactJS','https://reactjs.org/'],
+    ['React-TS','https://react-typescript-cheatsheet.netlify.app/'],
+    ['NextJS','https://nextjs.org/'],
+    ['Angular','https://angular.io/'],
+    ['Svelte','https://svelte.dev/'],
+    ['ExpressJs','https://expressjs.com/'],
+    ['Fastify','https://www.fastify.io/'],
+    ['NestJS','https://nestjs.com/'],
+    ['DotNet','https://dotnet.microsoft.com/'],
+    ['Spring','https://spring.io/'],
+    ['Laravel','https://laravel.com/'],
+    ['Django','https://www.djangoproject.com/'],
+    ['AdonisJS','https://adonisjs.com/'],
+    ['TallStack','https://tallstack.dev/'],
+]);
+
+export default links;
