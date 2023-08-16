3b9f51102fa79a4140d4663fdad9e09b2252079b
RuM <18311071+ru44@users.noreply.github.com>
2023-08-16 22:09:51 +0300

    feat: add example test

--- utils/questions.js
+++ utils/questions.js
@@ -1,73 +1,71 @@
 export const questions = {
-    DesignPattern:
-        {
-            type: 'list',
-            name: 'Design Pattern',
-            message: 'Choose the Design Pattern you want to use:',
-            choices: ['MVC', 'RESTful API'],
-        }
-    ,
-    Run:
-        {
-            type: 'confirm',
-            name: 'RunProject',
-            message: 'Do you want to run the project?',
-        }
-    ,
-    MVC:
-        {
-            type: 'list',
-            name: 'MVC',
-            message: 'Choose the MVC framework which you want to use:',
-            choices: ['TallStack', 'DjangoMTV'],
-        },
+    DesignPattern: {
+        type: 'list',
+        name: 'Design Pattern',
+        message: 'Choose the Design Pattern you want to use:',
+        choices: ['MVC', 'RESTful API'],
+    },
+    Run: {
+        type: 'confirm',
+        name: 'RunProject',
+        message: 'Do you want to run the project?',
+    },
+    MVC: {
+        type: 'list',
+        name: 'MVC',
+        message: 'Choose the MVC framework which you want to use:',
+        choices: ['TallStack', 'DjangoMTV'],
+    },
 
-    RestApiWebsite:
-        {
-            type: 'list',
-            name: 'frontend',
-            message: 'Choose the front framework which you want to use:',
-            choices: ['VueJs', 'NuxtJs', 'ReactJs', 'NextJs', 'Angular', 'Svelte'],
-        },
+    RestApiWebsite: {
+        type: 'list',
+        name: 'frontend',
+        message: 'Choose the front framework which you want to use:',
+        choices: ['VueJs', 'NuxtJs', 'ReactJs', 'NextJs', 'Angular', 'Svelte'],
+    },
 
-    RestApiMobile:
-        {
-            type: 'list',
-            name: 'mobile',
-            message: '( Only Flutter is available for now ):',
-            choices: ['Flutter'],
-        },
+    RestApiMobile: {
+        type: 'list',
+        name: 'mobile',
+        message: '( Only Flutter is available for now ):',
+        choices: ['Flutter'],
+    },
 
-    RestApiFrontend:
-        {
-            type: 'list',
-            name: 'typeOfFrontend',
-            message: 'is it a mobile App or Website:',
-            choices: ['Website', 'Mobile', 'Without Frontend'],
-        },
+    RestApiFrontend: {
+        type: 'list',
+        name: 'typeOfFrontend',
+        message: 'is it a mobile App or Website:',
+        choices: ['Website', 'Mobile', 'Without Frontend'],
+    },
 
-    RestApiBackend:
-        {
-            type: 'list',
-            name: 'backend',
-            message: 'Choose the backend framework which you want to use:',
-            choices: ['ExpressJs', 'NestJs', 'AdonisJs', 'Fastify', 'Django', 'Laravel', 'Spring Boot', 'ASP.NET', 'Without Backend'],
-        },
+    RestApiBackend: {
+        type: 'list',
+        name: 'backend',
+        message: 'Choose the backend framework which you want to use:',
+        choices: [
+            'ExpressJs',
+            'NestJs',
+            'AdonisJs',
+            'Fastify',
+            'Django',
+            'Laravel',
+            'Spring Boot',
+            'ASP.NET',
+            'Without Backend',
+        ],
+    },
 
-    PackageManager:
-        {
-            type: 'list',
-            name: 'packageManager',
-            message: 'Choose the package manager which you are using:',
-            choices: ['npm', 'yarn'],
-        },
+    PackageManager: {
+        type: 'list',
+        name: 'packageManager',
+        message: 'Choose the package manager which you are using:',
+        choices: ['npm', 'yarn'],
+    },
 
-    Database:
-        {
-            type: 'list',
-            name: 'database',
-            message: 'Choose the database which you want to use:',
-            choices: ['MongoDB', 'SQLite'],
-        }
-
+    Database: {
+        type: 'list',
+        name: 'database',
+        message: 'Choose the database which you want to use:',
+        choices: ['MongoDB', 'SQLite'],
+    },
 };
