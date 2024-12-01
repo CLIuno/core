81b4089871adea878a2d3cc37026ec03e2fc033f
Я♡M...∞ <18311071+ru44@users.noreply.github.com>
2024-12-01 11:07:45 +0300

    fix: commitlint

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,1 +1,4 @@
-export default {extends: ['@commitlint/config-conventional']}
+export default {
+    extends: ['@commitlint/config-conventional'],
+    ignores: [(commitMessage) => commitMessage.startsWith('Version Packages')],
+  };
